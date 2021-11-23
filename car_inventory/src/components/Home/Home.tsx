791c4e26c8d15a5abd9bcdd037b7156b6d54d197
nic0mp <nico.mp215@gmail.com>
2021-11-22 22:28:21 -0600

    + Dashboard and Sign iN

--- car_inventory/src/components/Home/Home.tsx
+++ car_inventory/src/components/Home/Home.tsx
@@ -1,13 +1,102 @@
 import React from 'react';
+import { makeStyles} from '@mui/styles';
+import { Button } from '@mui/material';
+import car_image from '../../assets/Images/car.png'
+// import {Link} from 'react-router-dom';
 
 interface Props{
     title: string;
 }
 
+//...interface above
+
+// New Make Styles Code
+const useStyles = makeStyles({
+    root:{
+        padding: '0',
+        margin: '0'
+    },
+    navbar_container: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    logo:{
+        margin: '0 0 0 0.45em'
+    },
+
+    logo_a: {
+        color: 'rgb(28,24,22)'
+    },
+    logo_navigation: {
+        listStyle: 'none',
+        textTransform: 'uppercase',
+        textDecoration: 'none'
+    },
+    navigation: {
+        display: 'flex'
+    },
+    nav_a:{
+        display: 'block',
+        padding: '1em',
+        color: 'black'
+    },
+
+    main: {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${car_image});`,
+        width: '100%',
+        height: '100%',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        position: 'absolute',
+    },
+    main_text:{
+        textAlign: 'center',
+        position: 'relative',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        color: 'white'
+    }
+    
+})
+
 export const Home = ( props:Props) => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            Hello World
+        <div className={classes.root}>
+            <nav>
+                <div className={classes.navbar_container}>
+                    <h1 className={`${classes.logo}`}>
+                        <a href="#" className={`${classes.logo_a} ${classes.logo_navigation}`}>Cars Cars Cars!</a>
+                    </h1>
+
+                    <ul className={`${classes.navigation} ${classes.logo_navigation}`}>
+                        <li>
+                            <a href=""  className={classes.nav_a}>Home</a>
+                        </li>
+                        <li>
+                            <a href=""  className={classes.nav_a}>About</a>
+                        </li>
+                        <li>    
+                            <a href=""  className={classes.nav_a}>Learn More</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main className={classes.main}>
+                <div className={classes.main_text}>
+                    <h1>{props.title}</h1>
+                    <p>Cars are Kewl</p>
+                    <Button color='primary' variant='contained'>Click Me</Button>
+                </div>
+
+            </main>
         </div>
+        
     )
+    
 }
