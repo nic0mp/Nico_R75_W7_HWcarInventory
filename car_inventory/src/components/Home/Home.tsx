3f4cc4868b6d9ed80a304ead518f4b29c3c4ce16
nic0mp <nico.mp215@gmail.com>
2021-11-22 20:49:57 -0600

    updates + images folder

---
+++ car_inventory/src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props{
+    title: string;
+}
+
+export const Home = ( props:Props) => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
